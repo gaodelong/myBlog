070512f885b7a91f0dfaf6dc1b519633b7ae5ab6
gaodelong <delonggao@126.cn>
2018-06-06 18:14:43 +0800

    去掉详情页

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/index'
-import ArticleDetails from '@/components/articleDetails'
+// import ArticleDetails from '@/components/articleDetails'
 
 Vue.use(Router)
 
@@ -12,10 +12,10 @@ export default new Router({
       name: '博客主页',
       component: Index
     },
-    {
-      path: '/ArticleDetails',
-      name: '文章内容页',
-      component: ArticleDetails
-    }
+    // {
+    //   path: '/ArticleDetails',
+    //   name: '文章内容页',
+    //   component: ArticleDetails
+    // }
   ]
 })
